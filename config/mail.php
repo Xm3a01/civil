773f1c4a05522721885b39a6511ff6c6acbfa429
Mohamed Amin <39481290+Xm3a01@users.noreply.github.com>
2019-09-29 21:23:25 +0200

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'mohamed29w@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'example@email.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
